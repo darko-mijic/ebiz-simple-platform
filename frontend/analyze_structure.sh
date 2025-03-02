1570a9f5901f775a4198f4bb9946393d722f7aaa
darko-mijic <darko.mijic@gmail.com>
2025-03-02 20:15:52 +0100

    Remove cached files that should be ignored

--- frontend/analyze_structure.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "======== FRONTEND DIRECTORY STRUCTURE ANALYSIS ========" && echo ""
-FRONTEND_DIRS=$(find . -name "frontend" -type d | sort) && FRONTEND_COUNT=$(echo "$FRONTEND_DIRS" | wc -l) && echo "Found $FRONTEND_COUNT frontend directories:" && echo "$FRONTEND_DIRS"
